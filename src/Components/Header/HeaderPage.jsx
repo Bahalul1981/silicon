c19f4f62bdf8366d6ee3aade6a2c5097d24a2d8d
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-05 15:34:23 +0100

    More style added in header

--- src/Components/Header/HeaderPage.jsx
+++ src/Components/Header/HeaderPage.jsx
@@ -4,38 +4,46 @@ import { Link } from "react-router-dom";
 
 function HeaderPage() {
   return (
-    <div className="main-header-div">
-      <div className="Logo-and-text">
-        <header>
-          <nav>
-            <ul>
-              <li>logo</li>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-              <li>
-                <Link to="/error">Error</Link>
-              </li>
-              <li>
-                <Link to="/features">Features</Link>
-              </li>
-              <li>
-                <Link to="/overview">Overview</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </div>
-      <div className="toggle-login">
-        <div>
-          <p>I am tougle</p>
+    <>
+      <header>
+        <div className="header-container">
+          <div className="logo-and-text">
+            <div className="logo-image">
+              <img src="" alt="Logo" />
+            </div>
+            <nav className="navigation-bar">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                  <Link to="/error">Error</Link>
+                </li>
+                <li>
+                  <Link to="/features">Features</Link>
+                </li>
+                <li>
+                  <Link to="/overview">Overview</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="toggle-login">
+            <div>
+              <p>I am toggle</p>
+            </div>
+            <div>
+              <p>I am toggle</p>
+            </div>
+          </div>
+          <button>Burger</button>
         </div>
-        <div>Login / logout</div>
-      </div>
-    </div>
+      </header>
+    </>
   );
 }
 
