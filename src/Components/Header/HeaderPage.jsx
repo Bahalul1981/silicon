72b55dd86d5d7a9383b0842d3d68f1700f1a0016
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-06 19:37:17 +0100

    Header style compleate

--- src/Components/Header/HeaderPage.jsx
+++ src/Components/Header/HeaderPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import "../../style/scss/style.min.css";
+import "../../style/css/style.min.css";
 import logos from "../../style/assets/Image/Group 2.png";
 import logoText from "../../style/assets/Image/Silicon.png";
-import signeUp from "../../style/assets/Image/btn.png";
-import Tougle from "../../style/assets/Image/light toggle.png";
+// import signeUp from "../../style/assets/Image/btn.png";
+// import Tougle from "../../style/assets/Image/light toggle.png";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 function HeaderPage() {
   return (
     <>
@@ -23,7 +24,7 @@ function HeaderPage() {
               </div>
             </div>
             <nav className="navigation-bar">
-              <ul>
+              <ul className="navigation-ul">
                 <li>
                   <Link to="/">Home</Link>
                 </li>
@@ -46,13 +47,19 @@ function HeaderPage() {
           <div className="toggle-login">
             <div className="Light-Dark">
               <div>Light</div>
-              <div>
-                <img src={Tougle} alt="" />
+              <div className="theme-switch">
+                <label>
+                  <input type="checkbox" />
+                  <span className="slider round"></span>
+                </label>
               </div>
               <div>Dark</div>
             </div>
             <div>
-              <img src={signeUp} className="Signe-up" alt="" />
+              <button className="login-logout">
+                <FontAwesomeIcon icon={faUser} className="fa-icon" />
+                Sign in / Sign up
+              </button>
             </div>
           </div>
 
