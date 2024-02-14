7c3e0773242641c9dd9c0c9568e2c1d34e3b2c28
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-14 20:01:23 +0100

    Main-page-Done

--- src/Components/Main/Home/HomePage.jsx
+++ src/Components/Main/Home/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import HeaderPage from "../../Header/HeaderPage";
+import appStore from "../../../style/assets/Image/appstore.svg";
+import playStore from "../../../style/assets/Image/googleplay.svg";
 import {
   AppFeature,
   HowDoesItWork,
@@ -10,30 +12,38 @@ import {
 import MobileImg from "../../../style/assets/Image/mobile.svg";
 function HomePage() {
   return (
-    <div>
+    <div className="bahalul">
       <HeaderPage />
       <div className="main-vid-homePage">
-        <div className="homepage-text-div">
-          <h1>Manage All Your Money in One App</h1>
-          <p>
-            We offer you a new feneration of the mobile banking.Save, spend &
-            manage money in your pocket.
-          </p>
+        <div className="container">
+          <div className="content">
+            <h1>Manage All Your Money in One App</h1>
+            <p>
+              We offer you a new feneration of the mobile banking.Save, spend &
+              manage money in your pocket.
+            </p>
 
-          <div className="apps-logo">
-            <div>
-              <img src="" alt="appStore" />
+            <div className="apps-logo">
+              <div>
+                <img src={appStore} alt="appStore" />
+              </div>
+              <div>
+                <img src={playStore} alt="googlePlay" />
+              </div>
             </div>
-            <div>
-              <img src="" alt="googlePlay" />
+            <div className="discover-more">
+              <a href="#">
+                <div className="round-cirkel">
+                  <i class="fa-solid fa-chevron-down"></i>
+                </div>
+                <span>Discover more</span>
+              </a>
             </div>
           </div>
-          <div className="discover-more-icon">
-            <img src="" alt="discoverMore" />
+
+          <div className="homepage-mobile">
+            <img src={MobileImg} alt="MobileImage" className="MobileImage" />
           </div>
-        </div>
-        <div className="homepage-image-div">
-          <img src={MobileImg} alt="MobileImage" />
         </div>
       </div>
       <AppFeature />
