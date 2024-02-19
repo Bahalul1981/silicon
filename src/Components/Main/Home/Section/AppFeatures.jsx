f2197a09d3d2054825d90b83cc9b2b225c103c26
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-19 16:00:42 +0100

    App feature has done

--- src/Components/Main/Home/Section/AppFeatures.jsx
+++ src/Components/Main/Home/Section/AppFeatures.jsx
@@ -5,6 +5,13 @@ import logo3 from "../../../../style/assets/Image/logo3.svg";
 import logo4 from "../../../../style/assets/Image/logo4.svg";
 import logo5 from "../../../../style/assets/Image/logo5.svg";
 import logo6 from "../../../../style/assets/Image/logo6.svg";
+import featureMobile from "../../../../style/assets/Image/feature/featuremobile.svg";
+import easyPayment from "../../../../style/assets/Image/feature/easyPayment.svg";
+import cost from "../../../../style/assets/Image/feature/cost.svg";
+import regular from "../../../../style/assets/Image/feature/regular.svg";
+import data from "../../../../style/assets/Image/feature/data.svg";
+import support from "../../../../style/assets/Image/feature/support.svg";
+import standerd from "../../../../style/assets/Image/feature/top.svg";
 
 function AppFeatures() {
   return (
@@ -29,37 +36,86 @@ function AppFeatures() {
           <img src={logo6} alt="logo6" />
         </a>
       </div>
+
       <div className="features-secton">
-        <div>
-          <img src="" alt="feature-mobile" />
+        <div className="feature-mobile">
+          <img src={featureMobile} alt="feature-mobile" />
         </div>
         <div className="feature-text">
           <div className="app-fetures">
             <h1>App Fetures</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
-              voluptatem perspiciatis, sequi odit aperiam voluptatibus eum eius
-              sapiente?{" "}
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+              natus. Neque placeat sapiente, nemo sequi odit magnam mollitia
+              eveniet temporibus ipsa nesciunt veritatis dolore laborum id,
+              cupiditate laboriosam.
             </p>
           </div>
           <div className="text-and-logo">
-            <div className="easyPayment">
-              <img src="" alt="easyPayment" />
+            <div className="text-and-logo-A">
+              <div className="easyPayment">
+                <img src={easyPayment} alt="" />
+                <div className="text">
+                  <h4>Easy Payments</h4>
+                  <p>
+                    Id mollis consectetur congue egestas egestas suspendisse
+                    blandit justo.
+                  </p>
+                </div>
+              </div>
+              <div className="easyPayment">
+                <img src={cost} alt="" />
+                <div className="text">
+                  <h4>Cost Statistics</h4>
+                  <p>
+                    Mattis urna ultricies non amet, purus in auctor non. Odio
+                    vulputate ac nibh.
+                  </p>
+                </div>
+              </div>
+              <div className="easyPayment">
+                <img src={regular} alt="" />
+                <div className="text">
+                  <h4>Regular Cashback</h4>
+                  <p>
+                    Sit facilisis dolor arcu, fermentum vestibulum arcu
+                    elementum imperdiet eleifend.
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="easyPayment">
-              <img src="" alt="dataSecurity" />
-            </div>
-            <div className="easyPayment">
-              <img src="" alt="costStatestic" />
-            </div>
-            <div className="easyPayment">
-              <img src="" alt="regularCashback" />
-            </div>
-            <div className="easyPayment">
-              <img src="" alt="support" />
-            </div>
-            <div className="easyPayment">
-              <img src="" alt="topStanderds" />
+            <div className="text-and-logo-B">
+              <div className="easyPayment">
+                <img src={data} alt="" />
+                <div className="text">
+                  <h4>Data Security</h4>
+                  <p>
+                    Augue pulvinar justo, fermentum fames aliquam accumsan
+                    vestibulum non.
+                  </p>
+                </div>
+              </div>
+              <div className="easyPayment">
+                <img src={support} alt="" />
+                <div className="text">
+                  <h4>Support 24/7</h4>
+                  <p>
+                    A elementum, imperdiet enim, pretium etiam facilisi in
+                    aenean quam mauris.
+                  </p>
+                </div>
+              </div>
+              <div className="easyPayment">
+                <img src={standerd} alt="" />
+                <div className="text">
+                  <h4>Top Standards</h4>
+                  <p>
+                    Faucibus cursus maecenas lorem cursus nibh. Sociis sit risus
+                    id. Sit facilisis dolor arcu.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
