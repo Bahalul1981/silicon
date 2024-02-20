6472a0b2dfe3b353061930a27e59c019c36dacaa
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-20 11:36:38 +0100

    How does it works done

--- src/Components/Main/Home/Section/HowDoesItWork.jsx
+++ src/Components/Main/Home/Section/HowDoesItWork.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import iphonProOne from "../../../../style/assets/Image/howDoesItWork/iPhonePro1.svg";
+import iphonProTwo from "../../../../style/assets/Image/howDoesItWork/iPhonePro2.svg";
+import iphonProThree from "../../../../style/assets/Image/howDoesItWork/iPhonePro3.svg";
 function HowDoesItWork() {
   return (
     <div className="how-does-it-work-main">
@@ -7,21 +9,35 @@ function HowDoesItWork() {
         <h1>How Does It Work</h1>
       </div>
       <div className="imageses">
+        <div className="iconOne">
+          <a href="#">
+            <div className="round-cirkel">
+              <i class="fa-solid fa-chevron-left"></i>
+            </div>
+          </a>
+        </div>
         <div className="imageOne">
-          <img src="" alt="mobileOne" />
+          <img src={iphonProOne} alt="mobileOne" />
         </div>
         <div className="imageTwo">
-          <img src="" alt="mobileTwo" />
+          <img src={iphonProTwo} alt="mobileTwo" />
         </div>
         <div className="imageThree">
-          <img src="" alt="mobileThree" />
+          <img src={iphonProThree} alt="mobileThree" />
+        </div>
+        <div className="iconTwo">
+          <a href="#">
+            <div className="round-cirkel">
+              <i class="fa-solid fa-chevron-right"></i>
+            </div>
+          </a>
         </div>
       </div>
       <div className="bottomText">
         <h3>Step 2. Latest transaction history </h3>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum minus
-          odio reprehenderit, fugiat assumenda voluptatum, aliquam dignissimos.
+          Enim, et amet praesent pharetra. Mi non ante hendrerit amet sed. Arcu
+          sociis tristique quisque hac in consectetur condimentum.
         </p>
       </div>
     </div>
