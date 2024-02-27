d2b1d8ee7f21bc606c3cb3c3f89de743bf40edc6
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-27 20:29:07 +0100

    contact page part1 done

--- src/Components/Main/Contact/ContactPage.jsx
+++ src/Components/Main/Contact/ContactPage.jsx
@@ -14,82 +14,80 @@ function ContactPage() {
       <div className="contactPage-wrapper">
         <HeaderPage />
 
+        <div className="contact-page-location">
+          <img src={home} alt="home" />
+          <img src={errow} alt="errow" />
+          <a href="">
+            <img src={date} alt="currentPage" />
+          </a>
+        </div>
         <div className="contact-page-main-div">
-          <div className="contact-page-location">
-            <img src={home} alt="home" />
-            <img src={errow} alt="errow" />
-            <a href="">
-              <img src={date} alt="currentPage" />
-            </a>
+          <div className="contactUs">
+            <div className="contactUs-h2">
+              <h2>Contact Us</h2>
+            </div>
+            <div className="email-us">
+              <div className="round-cirkel-big">
+                <img src={email} alt="emailLogo" />
+              </div>
+              <div className="email-us-text">
+                <h4>Email us</h4>
+                <p>
+                  Please feel free to drop us a line. We will <br></br> respond
+                  as soon as possible.
+                </p>
+                <a href="#">
+                  Leave a message{" "}
+                  <span>
+                    <i class="fa-solid fa-arrow-right"></i>
+                  </span>{" "}
+                </a>
+              </div>
+            </div>
+            <div className="Careers">
+              <div className="round-cirkel-big">
+                <img src={addHuman} alt="careersLogo" />
+              </div>
+              <div className="careers-text">
+                <h4>Careers</h4>
+                <p>
+                  Sit ac ipsum leo lorem magna nunc mattis <br></br> maecenas
+                  non vestibulum.
+                </p>
+                <a href="#">
+                  Send an application{" "}
+                  <span>
+                    <i class="fa-solid fa-arrow-right"></i>
+                  </span>{" "}
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="contact-form">
-            <div className="contactUs">
-              <div className="contactUs-h2">
-                <h2>Contact Us</h2>
+          <div className="contact-form-validation">
+            <h2>Get Online Consultation</h2>
+            <label htmlFor="fullName">Full name</label>
+            <input type="text" id="fullName" />
+            <label htmlFor="email">Email address</label>
+            <input type="text" id="email" />
+            <label htmlFor="specialist">Specialist</label>
+            <select name="" id="specialist">
+              <option value="1">Specialist1</option>
+              <option value="2">Specialist2</option>
+              <option value="3">Specialist3</option>
+            </select>
+
+            <div class="datetime-container">
+              <div class="date-time">
+                <label htmlFor="date">Date</label>
+                <input type="date" id="date" />
               </div>
-              <div className="email-us">
-                <div className="round-cirkel-big">
-                  <img src={email} alt="emailLogo" />
-                </div>
-                <div className="email-us-text">
-                  <h4>Email us</h4>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Fuga dicta ducimus officiis{" "}
-                  </p>
-                  <a href="#">
-                    Leave a message{" "}
-                    <span>
-                      <i class="fa-solid fa-arrow-right"></i>
-                    </span>{" "}
-                  </a>
-                </div>
-              </div>
-              <div className="Careers">
-                <div className="round-cirkel-big">
-                  <img src={addHuman} alt="careersLogo" />
-                </div>
-                <div className="careers-text">
-                  <h4>Careers</h4>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Fuga dicta ducimus officiis{" "}
-                  </p>
-                  <a href="#">
-                    Send an application{" "}
-                    <span>
-                      <i class="fa-solid fa-arrow-right"></i>
-                    </span>{" "}
-                  </a>
-                </div>
+              <div class="date-time">
+                <label htmlFor="time">Time</label>
+                <input type="time" id="time" />
               </div>
             </div>
-            <div className="contact-form-validation">
-              <h2>Get Online Consultation</h2>
-              <label htmlFor="fullName">Full name</label>
-              <input type="text" id="fullName" />
-              <label htmlFor="email">Email address</label>
-              <input type="text" id="email" />
-              <label htmlFor="specialist">Specialist</label>
-              <select name="" id="specialist">
-                <option value="1">Specialist1</option>
-                <option value="2">Specialist2</option>
-                <option value="3">Specialist3</option>
-              </select>
 
-              <div class="datetime-container">
-                <div class="date-time">
-                  <label htmlFor="date">Date</label>
-                  <input type="date" id="date" />
-                </div>
-                <div class="date-time">
-                  <label htmlFor="time">Time</label>
-                  <input type="time" id="time" />
-                </div>
-              </div>
-
-              <button className="button-primary">Make an appointment</button>
-            </div>
+            <button className="button-primary">Make an appointment</button>
           </div>
         </div>
       </div>
