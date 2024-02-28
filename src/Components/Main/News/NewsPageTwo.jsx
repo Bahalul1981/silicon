6ae08210cb3478a3e9a8c7c33ccee14252b63ea9
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-28 17:21:01 +0100

    API almost done

--- src/Components/Main/News/NewsPageTwo.jsx
+++ src/Components/Main/News/NewsPageTwo.jsx
@@ -1,12 +1,65 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import homeIcons from "../../../style/assets/Image/news/home.svg";
+import arrowIcons from "../../../style/assets/Image/news/bx-chevrons-right.svg";
+import newsIcons from "../../../style/assets/Image/news/News.svg";
 function NewsPageTwo() {
+  const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await fetch(
+          "https://kyhnet23-assignment.azurewebsites.net/api/news"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setNewsData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchNews();
+  }, []);
+
   return (
-    <div className="text-icon">
-      <img src="" alt="home-and-news-icon" />
-      <h2>Our News</h2>
+    <div className="news-page-wrapper">
+      <div className="text-icons">
+        <div className="news-icons">
+          <img src={homeIcons} alt="home" />
+          <img src={arrowIcons} alt="arrow" />
+          <a href="#">
+            <img src={newsIcons} alt="news" />
+          </a>
+        </div>
+        <div className="news-heading">
+          <h2>Our News</h2>
+        </div>
+      </div>
+
       <div className="web-api">
-        <h1>Here is web API</h1>
+        {newsData.map((news) => (
+          <div key={news.id} className="news-item">
+            <div className="upper-section">
+              <img src={news.imageUrl} alt="News" />
+              <div className="catagory-date">
+                <p className="catagory">{news.category}</p>
+                <p className="published">{news.published}</p>
+              </div>
+            </div>
+            <h4 className="news-title">{news.title}</h4>
+            <div className="horizontal-line">
+              <hr />
+            </div>
+
+            <div className="lower-section">
+              <img src={news.author.profileImageUrl} alt="Author" />
+
+              <p className="auther-name">{`${news.author.firstName} ${news.author.lastName}`}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
