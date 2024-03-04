69d4fe73815846e688b244506494607bed0d3408
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-03-04 19:07:50 +0100

    navbar has done for mobile version

--- src/Components/Header/HeaderPage.jsx
+++ src/Components/Header/HeaderPage.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import { useState } from "react";
 import "../../style/css/style.min.css";
 import logos from "../../style/assets/Image/SiliconLogo.svg";
 import logoText from "../../style/assets/Image/Silicon.png";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 function HeaderPage() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   const goToSigneInpage = () => {
     window.location.href = "/contact";
   };
@@ -24,28 +34,40 @@ function HeaderPage() {
                 <img src={logoText} alt="" />
               </div>
             </div>
-            <nav className="navigation-bar">
-              <ul className="navigation-ul">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/overview">Overview</Link>
-                </li>
-                <li>
-                  <Link to="/features">Features</Link>
-                </li>
-                <li>
-                  <Link to="/news">News</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                  <Link to="/error">Error</Link>
-                </li>
-              </ul>
-            </nav>
+            <div className={`navigation-bar ${menuOpen ? "open" : ""}`}>
+              <nav>
+                <ul className="navigation-ul">
+                  <li>
+                    <Link to="/" onClick={closeMenu}>
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/overview" onClick={closeMenu}>
+                      Overview
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/features" onClick={closeMenu}>
+                      Features
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/news" onClick={closeMenu}>
+                      News
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/contact" onClick={closeMenu}>
+                      Contact
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/error">Error</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
 
           <div className="toggle-login">
@@ -60,18 +82,25 @@ function HeaderPage() {
               <p>Dark</p>
             </div>
             <div>
-              <button className="login-logout" onClick={goToSigneInpage}>
+              <button className="button-primary" onClick={goToSigneInpage}>
                 <FontAwesomeIcon icon={faUser} className="fa-icon" />
                 Sign in / up
               </button>
             </div>
           </div>
 
-          <div className="burgerMenue">
-            <FontAwesomeIcon
-              icon={faBars}
-              style={{ fontSize: "24px", color: "black" }}
-            />
+          <div
+            className="burgerMenue"
+            style={{ cursor: "pointer" }}
+            onClick={toggleMenu}
+          >
+            <div style={{ fontSize: "24px", color: "black" }}>
+              {menuOpen ? (
+                <FontAwesomeIcon icon={faTimes} />
+              ) : (
+                <FontAwesomeIcon icon={faBars} />
+              )}
+            </div>
           </div>
         </div>
       </header>
